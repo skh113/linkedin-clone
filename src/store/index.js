ad8d39214009c2e5a3342c86521b7e96e275dcfe
Keyvan Hosseini <skhprogramming@gmail.com>
2022-08-30 17:18:30 +0430

    Added: Redux for "store" & "reducers"

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
-import { CreateStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from "../reducers";
 
-const store = CreateStore(rootReducer, {});
+const store = createStore(rootReducer, {});
 
 export default store;
